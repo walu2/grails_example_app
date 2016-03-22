063e1d7166b6e74aae837bd13075538703123c76
Piotr Walkowski <piotr@deluxe-soft.com>
2016-03-22 22:58:32 +0100

    Event domain added

--- grails-app/domain/com/tekdays/TekMessage.groovy
+++ grails-app/domain/com/tekdays/TekMessage.groovy
@@ -1,7 +1,18 @@
 package com.tekdays
 
 class TekMessage {
+	String subject
+	String content
+	TekMessage parent
+	TekEvent event
+	TekUser author
 
     static constraints = {
+		subject blank: false
+		content blank: false, maxSize: 2000
+		parent nullable: true
+		author nullable: false
     }
+	
+	static belongsTo = TekEvent
 }
