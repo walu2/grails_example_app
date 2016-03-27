0d915acd22b90307334260032a8df78ba6955caa
Piotr Walkowski <piotr@deluxe-soft.com>
2016-03-27 23:16:37 +0200

    Lets add some tests to Spec

--- src/test/groovy/com/tekdays/TaskServiceSpec.groovy
+++ src/test/groovy/com/tekdays/TaskServiceSpec.groovy
@@ -1,22 +1,34 @@
 package com.tekdays
-
 import grails.test.mixin.TestFor
 import spock.lang.Specification
-
 /**
- * See the API for {@link grails.test.mixin.services.ServiceUnitTestMixin} for usage instructions
- */
+* See the API for {@link grails.test.mixin.services.ServiceUnitTestMixin}
+* for usage instructions
+*/
 @TestFor(TaskService)
 class TaskServiceSpec extends Specification {
-
-    def setup() {
-    }
-
-    def cleanup() {
-    }
-
-    void "test something"() {
-        expect:"fix me"
-            true == false
-    }
+	def taskService
+	
+	def setup() {
+		new TekUser(fullName:'Tammy Tester', userName:'tester', 
+			email:'tester@test.com' , website:'test.com', 
+			bio:'A test person').save()
+	}
+	
+	def cleanup() {}
+	
+	void "test addDefaultTasks"() {
+		when: "we pass an event to taskService.addDefaultTasks"
+			def e = new TekEvent(name:'Test Event', city:'TestCity, USA',
+					description:'Test Description',
+					organizer:TekUser.findByUserName('tester' ),
+					venue:'TestCenter' ,
+					startDate:new Date(),
+					endDate:new Date() + 1)
+		
+		taskService.addDefaultTasks(e)
+		
+		then: "the event will have 6 default tasks" 
+			e.tasks.size() == 6
+	}
 }
